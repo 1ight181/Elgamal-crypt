cb1c5eb982efe816277bf399cc54050ef6195caf
Light <danil.odinzov181@gmail.com>
2024-11-18 17:20:07 +0500

    elgamal

---
+++ Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace Elgamal_crypt_для_конфы
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.publicKeyText = new System.Windows.Forms.TextBox();
+            this.privateKeyText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.messageText = new System.Windows.Forms.TextBox();
+            this.sessionalKeyText = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.createElgamalSysButton = new System.Windows.Forms.Button();
+            this.encryptButton = new System.Windows.Forms.Button();
+            this.decryptButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // publicKeyText
+            // 
+            this.publicKeyText.Location = new System.Drawing.Point(12, 38);
+            this.publicKeyText.Name = "publicKeyText";
+            this.publicKeyText.Size = new System.Drawing.Size(145, 23);
+            this.publicKeyText.TabIndex = 0;
+            // 
+            // privateKeyText
+            // 
+            this.privateKeyText.Location = new System.Drawing.Point(163, 38);
+            this.privateKeyText.Name = "privateKeyText";
+            this.privateKeyText.Size = new System.Drawing.Size(120, 23);
+            this.privateKeyText.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Открытый ключ (y, g, p)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(163, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Закрытый ключ (x)";
+            // 
+            // messageText
+            // 
+            this.messageText.Location = new System.Drawing.Point(12, 126);
+            this.messageText.Multiline = true;
+            this.messageText.Name = "messageText";
+            this.messageText.Size = new System.Drawing.Size(271, 105);
+            this.messageText.TabIndex = 4;
+            // 
+            // sessionalKeyText
+            // 
+            this.sessionalKeyText.Location = new System.Drawing.Point(12, 82);
+            this.sessionalKeyText.Name = "sessionalKeyText";
+            this.sessionalKeyText.Size = new System.Drawing.Size(127, 23);
+            this.sessionalKeyText.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Сесионный ключ (k)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Сообщение";
+            // 
+            // createElgamalSysButton
+            // 
+            this.createElgamalSysButton.Location = new System.Drawing.Point(303, 38);
+            this.createElgamalSysButton.Name = "createElgamalSysButton";
+            this.createElgamalSysButton.Size = new System.Drawing.Size(127, 68);
+            this.createElgamalSysButton.TabIndex = 8;
+            this.createElgamalSysButton.Text = "Создать систему Эль-гамаля";
+            this.createElgamalSysButton.UseVisualStyleBackColor = true;
+            this.createElgamalSysButton.Click += new System.EventHandler(this.createElgamalSysButton_Click);
+            // 
+            // encryptButton
+            // 
+            this.encryptButton.Location = new System.Drawing.Point(303, 126);
+            this.encryptButton.Name = "encryptButton";
+            this.encryptButton.Size = new System.Drawing.Size(127, 45);
+            this.encryptButton.TabIndex = 9;
+            this.encryptButton.Text = "Зашифровать";
+            this.encryptButton.UseVisualStyleBackColor = true;
+            this.encryptButton.Click += new System.EventHandler(this.encryptButton_Click);
+            // 
+            // decryptButton
+            // 
+            this.decryptButton.Location = new System.Drawing.Point(303, 186);
+            this.decryptButton.Name = "decryptButton";
+            this.decryptButton.Size = new System.Drawing.Size(127, 45);
+            this.decryptButton.TabIndex = 10;
+            this.decryptButton.Text = "Расшифровать";
+            this.decryptButton.UseVisualStyleBackColor = true;
+            this.decryptButton.Click += new System.EventHandler(this.decryptButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 257);
+            this.Controls.Add(this.decryptButton);
+            this.Controls.Add(this.encryptButton);
+            this.Controls.Add(this.createElgamalSysButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.sessionalKeyText);
+            this.Controls.Add(this.messageText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.privateKeyText);
+            this.Controls.Add(this.publicKeyText);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox publicKeyText;
+        private System.Windows.Forms.TextBox privateKeyText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox messageText;
+        private System.Windows.Forms.TextBox sessionalKeyText;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button createElgamalSysButton;
+        private System.Windows.Forms.Button encryptButton;
+        private System.Windows.Forms.Button decryptButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+
+namespace Elgamal_crypt_для_конфы
+{
+    public partial class Form1 : Form
+    {
+        //это прототип, вычисление первообразного корня не всегда верно,
+        //можно исправить, нужна факторизация и упрощение степеней
+        //но для исследования скорости шифрованяи достаточно, так как можно подставить готовые значения, не используя вычислений
+
+        //сколькими битами кодируется один символ, используется представление в UTF-16
+        const int charSize = 16;
+
+        //открытый ключ
+        int pNumber; //большое простое
+        int gNumber; //первообразный корень от p
+        int yNumber; //y=g^x mod p
+
+        //закрытый ключ
+        int xNumber; //1 < x < p - 1 
+
+        //сессионный ключ
+        int kNumber; //1 < k < p - 1; k - взаимнопростое с p-1
+
+        //шифротекст
+        int aNumber; //a = g^k mod p 
+
+        //рандомайзер
+        Random rnd = new Random();
+
+        int kf = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        //создание системы Эль-Гамаля
+        private void createElgamalSysButton_Click(object sender, EventArgs e)
+        {
+            //создаем открытый ключ(p, g, y) и закрытый (x)
+            pNumber = CreatePrime();
+            gNumber = CreatePrimitiveRoot(pNumber);
+            xNumber = rnd.Next(1, pNumber - 1);
+            yNumber = (int)(BigInteger.Pow(gNumber, xNumber) % pNumber); //y=g^x mod p
+
+            //выводим открытый ключ
+            publicKeyText.Text = yNumber.ToString() + ' ' + gNumber.ToString() + ' ' + pNumber.ToString();
+
+            //выводим закрытый ключ
+            privateKeyText.Text = xNumber.ToString();
+
+            //создаем сессионный ключ
+            kNumber = rnd.Next(256, pNumber - 1);
+
+            //выводим сессионный ключ
+            sessionalKeyText.Text = kNumber.ToString();
+
+            //считаем aNumber
+            aNumber = (int)(BigInteger.Pow(gNumber, kNumber) % pNumber);
+        }
+
+        //шифрование
+        private void encryptButton_Click(object sender, EventArgs e)
+        {
+            //берем значение сообщения
+            string message = messageText.Text;
+
+            //объявляем массив кодов для каждого из символов сообщения
+            int[] codes = new int[message.Length];
+
+            //переводим каждый символ в коды
+            for (int i = 0; i < message.Length; i++)
+            {
+                codes[i] = Convert.ToInt32(CharToBinaryFormat(message[i]), 2);
+
+            }
+
+            //объявляем массивы зашифрованных кодов для каждого из символов сообщения (a и b)
+            int[] bEncryptedCodes = new int[message.Length];
+
+            //объявляем строку зашифрованного сообщения
+            string encryptedStr;
+            messageText.Text = "";
+            messageText.Text = messageText.Text + StringFromBinaryToNormalFormat(Convert.ToString(aNumber * kf, 2));
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                bEncryptedCodes[i] = (int)((BigInteger.Pow(yNumber, kNumber) * codes[i]) % pNumber) * kf;
+                //messageText.Text = messageText.Text + (bEncryptedCodes[i] / kf).ToString();
+                encryptedStr = Convert.ToString(bEncryptedCodes[i], 2);
+                messageText.Text = messageText.Text + StringFromBinaryToNormalFormat(encryptedStr);
+            }
+        }
+
+        //расшифровка
+        private void decryptButton_Click(object sender, EventArgs e)
+        {
+            //берем значение сообщения
+            string message = messageText.Text;
+
+            //объявляем массив кодов для каждого из символов сообщения
+            int[] codes = new int[message.Length];
+
+            //переводим каждый символ в коды
+            for (int i = 0; i < message.Length; i++)
+            {
+                codes[i] = Convert.ToInt32(CharToBinaryFormat(message[i]), 2);
+            }
+
+            aNumber = (codes[0] / kf);
+            messageText.Text = "";
+            for (int i = 1; i < message.Length; i++)
+            {
+                messageText.Text = messageText.Text + StringFromBinaryToNormalFormat(Convert.ToString(((int)(codes[i] / kf * BigInteger.Pow(aNumber, pNumber - 1 - xNumber) % pNumber)), 2));
+            }
+        }
+
+        //метод возвращает истину если число простое, ложь в противном случае
+        private bool IsPrime(int primeNumber)
+        {
+            bool result = true;
+
+            for (int i = 2; i <= primeNumber / 2; i++)
+            {
+                if (primeNumber % i == 0)
+                {
+                    return result = false;
+                }
+            }
+            return result;
+        }
+
+        //метод генерирует простое число
+        private int CreatePrime()
+        {
+            int primeNumber = rnd.Next(512, 1024);
+
+            //перегенирируем до тех пор, пока число не будет простым
+            while (!IsPrime(primeNumber))
+            {
+                primeNumber = rnd.Next(512, 1024);
+            }
+
+            return primeNumber;
+        }
+
+        //метод возвращающий истину, если числа взаимопростые, ложь в противном случае
+        private bool IsCoprime(int firstNumber, int secondNumber)
+        {
+            if (firstNumber % secondNumber != 0 && secondNumber % firstNumber != 0)
+                return true;
+            else
+                return false;
+        }
+
+        //метод для нахождения всех делителей некоторого числа
+        private List<int> FindAllDividers(int dividend)
+        {
+            List<int> dividers = new List<int>(); 
+            for (int i = 2; i < dividend; i++)                
+                if (dividend % i == 0)
+                    dividers.Add(i);
+            return dividers;
+        }
+
+        //метод возвращающий истину, если число первообразный корень другого некоторого числа,
+        //ложь в противном случае
+        private bool IsPrimitiveRoot(int gNumber, int pNumber)
+        {
+            List<int> dividers = FindAllDividers(pNumber - 1);
+            BigInteger pNum = pNumber;
+            BigInteger gNum = gNumber;       
+
+            foreach (int divider in dividers)
+            {
+                if ((BigInteger.Pow(gNum, divider) - 1) % pNum == 0)
+                    return false;    
+            }
+            return true;
+        }
+
+        //возвращает первообразный корень по модулю pNumber, возвращает ноль, если такового нет
+        private int CreatePrimitiveRoot(int pNumber)
+        { 
+            for(int i = 256; i < pNumber; i++)
+            {
+                if (IsPrimitiveRoot(i, pNumber))
+                    return i;
+            }
+            return 0;
+        }
+
+        private string StringFromBinaryToNormalFormat(string str)
+        {
+            string output = "";
+
+            int resultNumber = 0;
+            int degree = str.Length - 1;
+
+            foreach (char ch in str)
+                resultNumber += Convert.ToInt32(ch.ToString()) * (int)Math.Pow(2, degree--);
+
+            output = ((char)resultNumber).ToString();
+
+            return output;
+        }
+
+        private string CharToBinaryFormat(char ch)
+        {
+            string out_str = "";
+
+            string char_binary = Convert.ToString(ch, 2);
+
+            while (char_binary.Length < charSize)
+                char_binary = "0" + char_binary;
+
+            out_str += char_binary;
+
+            return out_str;
+        }
+    }
+}
